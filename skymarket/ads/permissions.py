58c930db47591ee484a77f1079cae95f3f2f7f23
aperushin <aperushin@gmail.com>
2023-08-02 16:42:20 +0300

    Add permissions

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,22 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+from users.models import UserRoles
+
+
+class IsOwner(BasePermission):
+    message = "You do not have permission to perform this action."
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
+
+
+class IsAdmin(BasePermission):
+    message = "You do not have permission to perform this action."
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == UserRoles.ADMIN
--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -2,10 +2,12 @@ from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from ads.models import Ad, Comment
 from ads.filters import AdFilterSet
 from ads.serializers import AdSerializer, AdDetailSerializer, AdCreateSerializer, CommentSerializer
+from ads.permissions import IsAdmin, IsOwner
 
 
 class AdPagination(PageNumberPagination):
@@ -17,21 +19,33 @@ class AdViewSet(ModelViewSet):
     querysets = {
         "retrieve": Ad.objects.select_related("author"),
     }
+
     default_serializer = AdSerializer
     serializer_classes = {
         "retrieve": AdDetailSerializer,
         "create": AdCreateSerializer,
         "partial_update": AdCreateSerializer,
     }
+
     pagination_class = AdPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = AdFilterSet
+
+    default_permissions = [IsOwner | IsAdmin]
+    permissions = {
+        'list': [AllowAny],
+        'retrieve': [IsAuthenticated],
+    }
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.default_serializer)
 
     def get_queryset(self):
         return self.querysets.get(self.action, self.queryset)
+
+    def get_permissions(self):
+        self.permission_classes = self.permissions.get(self.action, self.default_permissions)
+        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -40,6 +54,7 @@ class AdViewSet(ModelViewSet):
 
 class MyAdListView(ListAPIView):
     serializer_class = AdSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Ad.objects.filter(author=self.request.user)
@@ -48,8 +63,18 @@ class MyAdListView(ListAPIView):
 class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
 
+    default_permissions = [IsOwner | IsAdmin]
+    permissions = {
+        'list': [AllowAny],
+        'retrieve': [IsAuthenticated],
+    }
+
     def get_queryset(self):
         return Comment.objects.filter(ad_id=self.kwargs['ad_pk']).select_related("author", "ad")
+
+    def get_permissions(self):
+        self.permission_classes = self.permissions.get(self.action, self.default_permissions)
+        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, ad_id=self.kwargs['ad_pk'])
