ac5533d92aae10e2908c1cdf07773421989058f1
aperushin <aperushin@gmail.com>
2023-08-02 17:43:29 +0300

    Fix CustomUserAdmin class

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role')
-    list_filter = ('role', 'is_active')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+    list_display = ("email", "first_name", "last_name", "role")
+    list_filter = ("role", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
     filter_horizontal = ()
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone", "image")}),
+        (_("Permissions"), {
+            "fields": ("is_active", "role"),
+        }),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
