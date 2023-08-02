015741bb90290ecd9fd4b6b85406f6d6fd291f3a
aperushin <aperushin@gmail.com>
2023-08-02 17:22:19 +0300

    Add models to admin panel

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -3,4 +3,13 @@ from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
-# admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role')
+    list_filter = ('role', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
